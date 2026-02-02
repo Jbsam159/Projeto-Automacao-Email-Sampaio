ba3b1f177ae18185e6e8a9b39e608aeb3c5b0211
JoaoVictor <jbsam159@gmail.com>
2026-02-02 09:37:09 -0300

    chore: adaptacoes para deploy do frontend

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000", // backend FastAPI
+  baseURL: import.meta.env.VITE_API_URL, // backend FastAPI
 });
 
